ec2fe9a19073f643b4c5a44b25c5feaf88fac5e2
Rute Martins <rute@mitre.org>
2020-04-03 15:56:13 -0500

    add procedure

--- schemas/index.js
+++ schemas/index.js
@@ -4,11 +4,16 @@ const patient_identifiable_information = require('./patient_identifiable_informa
 const c19_condition = require('./c19_condition.json');
 const allergy_intolerance = require('./allergy_intolerance.json');
 const medication_administration = require('./medication_administration.json');
+const smoking_status = require('./smoking_status.json');
+const procedure = require('./procedure.json');
 
 module.exports = [
   patient,
   patient_vital_status,
   patient_identifiable_information,
   c19_condition,
-  medication_administration
+  medication_administration,
+  allergy_intolerance,
+  smoking_status,
+  procedure
 ];
