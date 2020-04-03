954563a2e8bd84ff9ef47cb79018c91e54f92519
Rute Martins <rute@mitre.org>
2020-04-03 14:12:45 -0500

    add allergy_intolerance

--- schemas/index.js
+++ schemas/index.js
@@ -1,11 +1,13 @@
 const patient = require('./patient.json');
-const patient_status = require('./patient_vital_status.json');
-const patient_identifiable_information = require('./patient__identifiable_information.json');
+const patient_vital_status = require('./patient_vital_status.json');
+const patient_identifiable_information = require('./patient_identifiable_information.json');
 const c19_condition = require('./c19_condition.json');
+const allergy_intolerance = require('./allergy_intolerance.json')
 
 module.exports = [
   patient,
   patient_vital_status,
-  patient_identifiable_identification,
+  patient_identifiable_information,
   c19_condition,
+  allergy_intolerance
 ];
